7c1237abbbd9fa876fcf8e3243e4c0d989c0ba56
AsminSayim <145451871+AsminSayim@users.noreply.github.com>
2023-11-20 10:42:18 +0300

    Add files via upload

---
+++ break-ornk/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace break_ornk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAra = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(52, 77);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 23);
+            this.btnAra.TabIndex = 0;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Mehmet",
+            "Ali",
+            "Halil",
+            "Deniz",
+            "Ahmet",
+            "Yusuf",
+            "Mehmet",
+            "Emre",
+            "Melek",
+            "Beyza"});
+            this.listBox1.Location = new System.Drawing.Point(187, 77);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 277);
+            this.listBox1.TabIndex = 1;
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(52, 117);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(100, 20);
+            this.txtAra.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 441);
+            this.Controls.Add(this.txtAra);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnAra);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtAra;
+    }
+}
+
---
+++ break-ornk/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace break_ornk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string aranan = txtAra.Text;
+            bool kontrol= false;
+
+            for (int i = 0; i < listBox1.Items.Count; i=i+10)
+            {
+                if(aranan == listBox1.Items[i].ToString())
+                {
+                    kontrol=true;
+                    break;
+                }
+            }
+            if (kontrol == true)
+            {
+                MessageBox.Show("Öğrenci bulundu.");
+            }
+            else
+            {
+                MessageBox.Show("Öğrenci bulunamadı.");
+            }
+            
+            
+        }
+    }
+}
